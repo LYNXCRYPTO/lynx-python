94109c9044b3050d8c860e6ac8fa571bbbf06500
Jordan Lesson <jordanlesson@Jordans-MBP-2.hsd1.mi.comcast.net>
2022-03-31 11:22:18 -0400

    message signing and verification added w/ SHA3_256

--- lynx/account.py
+++ lynx/account.py
@@ -1,10 +1,16 @@
 # account.py
 
-from node import display_debug
+from hashlib import sha3_256
+from message import Message, SignedMessage
 from Crypto.PublicKey import RSA
-from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
-from Crypto.Hash import SHA256
+import ecdsa
 import binascii
+import threading
+
+
+def display_debug(msg):
+    """Prints a message to the screen with the name of the current thread"""
+    print("[%s] %s" % (str(threading.currentThread().getName()), msg))
 
 
 class Account:
@@ -16,12 +22,17 @@ class Account:
     # ------------------------------------------------------------------------------
     def __init__(self) -> None:
         # --------------------------------------------------------------------------
-        """Generates a 1024-bit RSA key_pair made up of a public and private key"""
+        """Generates a 1024-bit RSA key pair made up of a public and private key"""
 
         self.debug = 1
 
         self.key_pair = RSA.generate(bits=1024)
-        self.public_key = self.key_pair.publickey()
+        self.public_key = (self.key_pair.n, self.key_pair.e)  # Key pair (n, e)
+        self.__debug('Account Created!')
+        self.__debug('Public Key: (n: %s, e: %s)' %
+                     (hex(self.key_pair.n), hex(self.key_pair.e)))
+        self.__debug('Private Key: (n: %s, d: %s)\n' %
+                     (hex(self.key_pair.n), hex(self.key_pair.d)))
 
         self.nonce = 0
         self.balance = 0
@@ -35,37 +46,46 @@ class Account:
             display_debug(message)
 
     # ------------------------------------------------------------------------------
-    def sign_message(self, message) -> bytes:
+    def sign_message(self, message: Message) -> SignedMessage:
         # --------------------------------------------------------------------------
-        """Signs a message using the PKCS#1 v1.5 signature scheme (RSASP1)
-        and returns the signature as binary data.
+        """Signs a message using the SHA3-256 signature scheme and returns 
+        the signature with the message as a SignedMessage object.
         """
 
-        binary_message = message.encode('utf-8')
-        message_hash = SHA256.new(binary_message)
-        signer = PKCS115_SigScheme(self.key_pair)
-        signature = signer.sign(message_hash)
-        self.__debug('Signature: %s' % binascii.hexlify(signature))
-        return signature
+        message_JSON = message.to_JSON()
+        message_binary = message_JSON.encode()
+        message_hash = int.from_bytes(
+            sha3_256(message_binary).digest(), byteorder='big')
+        signature = pow(message_hash, self.key_pair.d, self.key_pair.n)
+        signed_message = SignedMessage(message=message, signature=signature)
+        self.__debug('Signature: %s' % hex(signed_message.signature))
+
+        return signed_message
 
     # ------------------------------------------------------------------------------
-    def verify_signature(self, message, sender_public_key, signature) -> bool:
+    def verify_signature(self, signed_message: SignedMessage, sender_public_key) -> bool:
         # --------------------------------------------------------------------------
-        """Verifies that the signature corresponding to the given message is a 
-        valid PKCS#1 v1.5 signature (RSAVP1)
+        """Verifies that the signature corresponding to the given message is a
+        valid SHA3-256 signature.
         """
+        if signed_message.is_signed:
+            message_JSON = signed_message.message.to_JSON()
+            message_binary = message_JSON.encode()
+            message_hash = int.from_bytes(
+                sha3_256(message_binary).digest(), byteorder='big')
+            hash_from_signature = pow(
+                signed_message.signature, sender_public_key[1], sender_public_key[0])
 
-        binary_message = message.encode('utf-8')
-        message_hash = SHA256.new(binary_message)
-        verifier = PKCS115_SigScheme(sender_public_key)
-        try:
-            verifier.verify(message_hash, signature)
-            self.__debug('Signature is valid')
-        except:
-            self.__debug("Signature is invalid.")
-            return False
+            if message_hash == hash_from_signature:
+                self.__debug('Signature is valid')
+            else:
+                self.__debug('Signature is invalid')
 
-        return True
+            return message_hash == hash_from_signature
+
+        self.__debug('Signature is of None value')
+
+        return False
 
 
 # end Account class
