189df21b7e997a9615daa2f3ab998e62a13634c7
ncs5306 <nicksaros07@gmail.com>
2022-06-05 13:43:32 -0400

    Added get balance function in accounts

--- lynx/accounts.py
+++ lynx/accounts.py
@@ -1,16 +1,27 @@
 import random
+import glob
+import json
 
 class Accounts:
     def __init__(self):
-        #should point to the most recent distributed state chain
-        self.balance = None
-
-        #512 bit random generated string
         self.address = hex(random.getrandbits(512))
-
-        #set to empty string by default 
         self.contractCode = ""
-
-        #Storage set to None as per the Lynx whitepaper
         self.storage = None
+        self.balance = self.getBalance()
+
+
+    def getBalance(self):
+        #sets the directory which the state chain .dat files are located
+        directory = f"./accounts/{self.address}/states"
+
+
+        #itterates through the files 
+        for filename in glob.iglob(f'{directory}/*'):
+
+            #opens as json and reads the balance of the state chain object
+            with open(f"{filename}") as datfile:
+                data = json.load(datfile)
+
+        #returns most recent blance which is set to the self.balance attribute
+        return data["balance"]
 
